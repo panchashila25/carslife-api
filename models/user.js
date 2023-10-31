0418a2758229054574e16733f8130fd322ac4cd9
krushnadeshmukh <krishnadeshmukh176@gmail.com>
2023-10-31 12:21:30 +0530

    updated

--- models/user.js
+++ models/user.js
@@ -6,6 +6,46 @@
     const User = mongoose.model(
       "User",
       new mongoose.Schema({
+        profile:{
+          type:String,
+          required:true
+        },
+        name:{
+          type:String,
+          required:true
+        }
+        ,
+        mobile:{
+          type:String,
+          required:true
+        },
+        email:{
+          type:String,
+          required:true
+        },
+        age:{
+          type:String,
+          required:true
+        },
+        userDetails:[{
+          address:{
+            type:String,
+            required:true
+          },
+          govId:{
+            type:String,
+            required:true
+          },
+          pickUp:{
+            type:String,
+            required:true
+          },
+          drop:{
+            type:String,
+            required:true
+          }
+        }]
+        
         
         
       }, {
